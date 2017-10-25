f75cbe79b70497dafcd4b5b71771ce3dc84bf8fe
sha6422 <33008054+sha6422@users.noreply.github.com>
2017-10-25 11:26:09 -0400

    Update sampling_the_data.py
    I changed the code for python3.

--- sampling_the_data.py
+++ sampling_the_data.py
@@ -7,28 +7,28 @@ a0 = 0
 
 ## create original set
 for i in range(len(a)):
-    b[i] = range(a0,a0+a[i])
+    b[i] = list(range(a0,a0+a[i]))
     a0 += a[i]
 
 ## arrange 5*(a[i]/5) elements
 for j in range(5):
     for i in range(len(a)):
         random.seed(1)
-        x = random.sample(b[i],a[i]/5)
+        x = random.sample(b[i],int(a[i]/5))
             
         for key in x:
             b[i].remove(key)
             c[j].append(key)
 
 ## arrange the rest elements       
-b.sort(lambda x,y: cmp(len(x), len(y)),reverse=True)   
+b.sort(key=len, reverse=True)   
 for key in b:
     random.seed(1)
     random.shuffle(key)
-    for i in range(len(key)):
+    for i in list(range(len(key))):
         c[i].append(key[i])
-    c.sort(lambda x,y: cmp(len(x), len(y)))
+    c.sort(key=len)
 
 for key in c:
-    key.sort(lambda x,y:cmp(x,y))
-    print key
+    key.sort()
+    print(key)
