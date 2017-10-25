eb592408282c64696ed39fb6b638d6fd7e80ebee
penny234 <pennyc523@gmail.com>
2017-10-24 23:43:04 -0400

    update with getcox_oct24.py in local file
    The getcox.py upload oct 21  has a bug that did not consider the tied events.
    1. At time event_time[25] ('event_free_survival_time_days'=650) and event_time[47] ('event_free_survival_time_days'=1604) have tied events. ls=[m,d,who] (at risk number, died number, event_subject_id)
     ls[25] / ls [47]  have tied events [167, 2, [15, 92]]/  [124, 2, [85, 93]];
    2. Add n_bins as a parameter

--- getcox.py
+++ getcox.py
@@ -1,77 +1,86 @@
-#latest working version 
+#this is the debugged version of getting the cox-score 
 import pandas as pd
 import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
 
-def h(inputdata):
-    
+
+def h(inputdata,n_bins):
+    n_bins=int(n_bins)
 #1. get inputdata
 #2. get event time
     event_time=inputdata[inputdata['death']==1]['event_free_survival_time_days'].sort_values().unique()
-    
     cols = ['V{0}'.format(element) for element in range(1,21149)] #has 21148 genes generate the genenames
-
+    
     ls=[]
     for i in event_time:
         m=(inputdata['event_free_survival_time_days']>i).sum()
         d=(inputdata['event_free_survival_time_days']==i).sum()
         who=inputdata.index[inputdata['event_free_survival_time_days']==i].tolist()
         ls.append([m,d,who])
-    
-#the index of at risk set 
-    indx=[[] for _ in range(len(event_time))]
+        
+    #the index of at risk set 
+    indx=[]
     for i in range(len(event_time)):
-        indx[i]=inputdata[inputdata['event_free_survival_time_days']>event_time[i]].index.values.tolist()
+        indx.append(inputdata[inputdata['event_free_survival_time_days']>event_time[i]].index.values.tolist())
+ 
 
-    
-#get Xik
-    sum_gene_val=pd.DataFrame(0, index=cols, columns=np.array(range(len(event_time))))
+ #get Xik
+    sum_gene_val=[]
     for i in range(len(event_time)): 
-        sum_gene_val.loc[:,i]=inputdata.loc[indx[i],cols].sum()
-
+        sum_gene_val.append(inputdata.loc[indx[i],cols].sum())
+        
 #get Xik_bar
     ser1=[item[0] for item in ls]
-    func1 = lambda x: np.asarray(x) / np.asarray(ser1)
-    avg_gene_val=sum_gene_val.apply(func1, axis=1)
-
-
+    avg_gene_val=[]
+    for i in range(len(sum_gene_val)):
+        avg_gene_val.append(sum_gene_val[i]/ser1[i])
+        
+        
 #get X*ik
     subj_time=[item[2] for item in ls]
-    subj_gene_val=pd.DataFrame(0, index=cols, columns=np.array(range(len(event_time))))
+    subj_gene_val=[]
+
     for i in range(len(event_time)):    
-        subj_gene_val.loc[:,i]=inputdata.loc[subj_time[i][0],cols]  #*bug
-    
-#get dk*Xik_bar
+        subj_gene_val.append(inputdata.loc[subj_time[i],cols].sum())     
+        
+        
+ #get dk*Xik_bar
+    davg_gene_val=[]
     ser2=[item[1] for item in ls]
-    func2 = lambda x: np.asarray(x)*np.asarray(ser2)
-    davg_gene_val=avg_gene_val.apply(func2, axis=1)
-
-
-
-#get ri
-    r=davg_gene_val.subtract(subj_gene_val, axis='columns').sum(axis=1)
-
-    #get Si
-    S_gene_val=pd.DataFrame(0, index=cols, columns=np.array(range(len(event_time))))
-    temp=inputdata.loc[:,cols].T
-    for i in range(len(event_time)):
-        S_gene_val.loc[:,i]=(temp.loc[:,indx[i]].sub(avg_gene_val.loc[:,i],axis=0)**2).sum(axis=1)*np.asarray(ser2[i])/np.asarray(ser1[i])
-
+    for i in range(len(ser2)):
+        davg_gene_val.append(avg_gene_val[i]*ser2[i])
+        
+ #get ri
+    r=pd.DataFrame(0,index=cols,columns=np.array(range(len(event_time))))
+    for i in range(len(ser2)):
+        r.loc[:,i]=(subj_gene_val[i]-davg_gene_val[i])
+        
+    ri=r.sum(axis=1)
     
+ #get Si
+    Tempi=[]
+    for i in range(len(event_time)): 
+        Tempi.append(inputdata.loc[indx[i],cols])
+    series_avg_gene_val=pd.Series(avg_gene_val[0])
+    S_gene_val=pd.DataFrame(0,index=cols,columns=np.array(range(len(event_time))))
+    for i in range(len(event_time)):
+        S_gene_val.loc[:,i]=(Tempi[i].sub(series_avg_gene_val)**2).sum()*ser2[i]/ser1[i]
+        
     si=S_gene_val.sum(axis=1)**(1/2)
-    s0=si.median()    
-    hi=np.asarray(r)/(np.asarray(si)+s0)
-    hi=np.absolute(hi)  #get absolute value of cox-score
-
-    stats.describe(hi) #mean=1.1501915210128424, variance=0.2270225498339461,  minmax=(2.8752108009228312e-06, 4.0052250126097571)
-    np.histogram(hi)   
-    plt.hist(hi,bins=20)
+    s0=si.median()
+ 
+ #get hi   
+    hi=ri.div(si+s0)
+    hi=np.absolute(hi)
+    
+    plt.hist(hi,bins=n_bins)
     plt.title("Histogram of absolute value of cox-score")
     plt.show()
 
-    return hi
 
+  
+    return hi
 
 
 
