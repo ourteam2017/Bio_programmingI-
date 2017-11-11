b36a5fb0b80daadb5a3774619eef444dac3833a0
Mario Barbé Ábrigo <mebarbe@uc.cl>
2017-11-10 20:04:12 -0500

    merged with clinical data and coded cols

--- genefilter.R
+++ genefilter.R
@@ -1,23 +1,20 @@
 #install.packages('matrixStats')
 #source("http://bioconductor.org/biocLite.R")
 #biocLite()
-biocLite("affy")
-biocLite("limma")
-biocLite("hgu133plus2.db")
-biocLite("annotate")
-biocLite("genefilter")
-install.packages("purrr")
+#biocLite("affy")
+#biocLite("limma")
+#biocLite("hgu133plus2.db")
+#biocLite("annotate")
+#biocLite("genefilter")
+#install.packages("purrr")
 
-library(data.table)
-library(affy)
-library(hgu133plus2.db)
-library(annotate)
-library(purrr)
-library(genefilter)
-library(limma)
-library(matrixStats)
+require(data.table)
+require(affy)
+require(hgu133plus2.db)
+require(annotate)
+require(purrr)
+require(genefilter)
 
-setwd('/Users/Penny/Desktop/dataset/ALL_P1')
 #this will read in all the *.CEL files in the working directory 
 affydata <- ReadAffy()
 # Normalization
@@ -70,6 +67,8 @@ quantiles_gene<- unique(long_dt_gene_name[, .(gene_name,
                                                p75,
                                                MAX)])
 
+
+# Plot distribution ###
 layout(mat = matrix(c(1,2),2,1, byrow=TRUE),  height = c(1,8))
 par(mar=c(0, 3.1, 1.1, 2.1))
 boxplot(long_dt_gene_name$MEDIAN, horizontal=TRUE, ylim=c(2.5,14), xaxt="n" ,col=rgb(0.8,0.8,0,0.5) , frame=F, outcex=0.5)
@@ -87,5 +86,70 @@ hist(long_dt_gene_name$Avg,xaxt='n',xlab='average expression level of genes',yla
 axis(side=1, at=seq(2.5,13.5, 0.5))
 abline(v=median(long_dt_gene_name$Avg),col='red')
 text(4.93, y=0, '4.93',pos=4, cex=0.8) 
+
+
+# Read clinical data ####
+require(readxl)
+
+clinical_key <- read_excel('TARGET_ALL_GeneExpressionArray_Phase1_20160812.xlsx', sheet = 2) %>%
+  setDT(.) %>%
+  .[, c(1,36)] %>%
+  setnames(., c('patient_id', 'cel_file')) %>%
+  unique(.)
+
+clinical <- read_excel('TARGET_ALL_ClinicalData_Phase1_20160714.xlsx', sheet = 1) %>%
+  setDT(.) %>%
+  .[, c(1:8,13:15,18)] %>%
+  setnames(., c('patient_id', 'gender', 'race', 'ethnicity', 'age_diagnosis_days',
+                'first_event', 'event_free_survival_time_days', 'vital_status',
+                'wbc_diagnosis', 'cns_status', 'testicular_involment', 'mrd_29')) %>%
+  clinical_key[., on = 'patient_id'] %>%
+  .[!is.na(cel_file),]
+
+
+# Creat categorical values of key clinical features####
+require(purrr)
+require(data.table)
+
+clinical[, `:=` (snc_teste_involve = 0,
+                 latino_black = 0,
+                 under_1_over_10_year = 0,
+                 wbc_over_50k = 0,
+                 mrd_29_positive = 0,
+                 dead = 0)] %>%
+  .[cns_status %in% c('CNS 2', 'CNS 3') | testicular_involment == 'Yes',
+         snc_teste_involve := 1] %>%
+  .[cns_status == 'CNS 1' & testicular_involment == 'Unknown',
+         snc_teste_involve := NA] %>%
+  .[race == 'Black or African American' | ethnicity == 'Hispanic or Latino',
+         latino_black := 1] %>%
+  .[race == 'Unknown' & ethnicity == 'Unknown',
+         latino_black := NA] %>%
+  .[age_diagnosis_days < 365 | age_diagnosis_days > 3650,
+         under_1_over_10_year := 1] %>%
+  .[wbc_diagnosis > 50, wbc_over_50k := 1] %>%
+  .[mrd_29 > 0, mrd_29_positive := 1] %>%
+  .[is.na(mrd_29), mrd_29_positive := NA] %>%
+  .[vital_status == 'Dead', dead := 1] %>%
+  .[vital_status %in% c('Unknown', 'Unspecified'), dead := NA] %>%
+
+  # Make key for join with gene expression data set
+  .[, B := 1:.N]
+
+clinical <- clinical[, c(1:3,7:9,14:20)]
+# Join clinical data with gene expression data ####
+
+# Disable package purrr, so you can use data.table transpose function
+#detach('package:purrr', unload = T)
+
+# Prefered to explicit call a function from a package
+cda <- data.table::transpose(dt_gene_names)
+
+cda[, B := -2:.N]
+
+join <- clinical[cda, on = 'B']
+
+fwrite(join, 'clinical_gene.csv', sep = '|')
+
 
 
